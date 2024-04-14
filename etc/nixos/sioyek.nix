4b3d1a48be7dd9c3390af205d8ecef28a2fa4df6
Hemant Sai Gouni <me@hgouni.com>
2024-04-14 05:38:53 +0000

    Fix issues with sioyek config

--- etc/nixos/sioyek.nix
+++ etc/nixos/sioyek.nix
@@ -9,12 +9,12 @@
     config = {
       "custom_text_color" = "#ebdbb2";
       "custom_background_color" = "#282828";
-      "status_bar_text_color" = "#ebdbb2";
+      "ui_text_color" = "#ebdbb2";
       "ui_background_color" = "#504945";
       "ui_selected_text_color" = "#282828";
       "ui_selected_background_color" = "#ebdbb2";
-      "text_highlight_color" = "#504945";
-      "visual_mark_color" = "#504945";
+      "visual_mark_color" = "#fabd2f";
+      "text_highlight_color" = "#fabd2f";
       "search_highlight_color" = "#fabd2f";
       "link_highlight_color" = "#fabd2f";
       "synctex_highlight_color" = "#fabd2f";
