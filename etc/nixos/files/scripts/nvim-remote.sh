b1379322172eaad1f35b45c3b8e6d11f802b09d9
Hemant Sai Gouni <me@hgouni.com>
2024-06-08 03:28:49 +0000

    Set $EDITOR correctly; handle errors

--- etc/nixos/files/scripts/nvim-remote.sh
+++ etc/nixos/files/scripts/nvim-remote.sh
@@ -1,7 +1,11 @@
-# double quoting $@ is special: quotes each entry in the list of arguments
-# separate by null terminators instead of newlines to handle files w/ spaces
-readlink --canonicalize --zero "$@" \
-    | xargs --null nvim --server "$NVIM" --remote-tab
+if [ -n "$NVIM" ] && [ "$#" -gt 0 ]; then
+    # double quoting $@ is special: quotes each entry in the list of arguments
+    # separate by null terminators instead of newlines to handle files w/ spaces
+    readlink --canonicalize --zero "$@" \
+        | xargs --null nvim --server "$NVIM" --remote-tab
 
-# so the 'editor' waits for input
-read -rp $'Press enter to finish editing (for temporary files).\n'
+    # so the 'editor' waits for input
+    read -rp $'Press enter to finish editing (for temporary files).\n'
+else
+    printf '%s\n' 'No arguments given, or not running inside nvim. Exiting.'
+fi
