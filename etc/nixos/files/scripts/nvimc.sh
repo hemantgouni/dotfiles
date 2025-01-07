aef11f0cfe44fffc4922cc1cef93739e8197195f
Hemant Sai Gouni <me@hgouni.com>
2025-01-07 01:20:53 +0000

    Set $SSH_AUTH_SOCK in nvim server

--- etc/nixos/files/scripts/nvimc.sh
+++ etc/nixos/files/scripts/nvimc.sh
@@ -1,2 +1,3 @@
+nvim --server "localhost:${1:-9090}" --remote-expr "setenv('SSH_AUTH_SOCK', '$SSH_AUTH_SOCK')" > /dev/null 2>&1
 # accept an argument for the port, or use 9090 as the default
 nvim --server "localhost:${1:-9090}" --remote-ui
