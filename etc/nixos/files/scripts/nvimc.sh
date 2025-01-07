3dd5d9c1067698aed8c85649fd21c79208cad888
Hemant Sai Gouni <me@hgouni.com>
2025-01-07 07:31:21 +0000

    Use --headless rather than null redirect

--- etc/nixos/files/scripts/nvimc.sh
+++ etc/nixos/files/scripts/nvimc.sh
@@ -1,3 +1,3 @@
-nvim --server "localhost:${1:-9090}" --remote-expr "setenv('SSH_AUTH_SOCK', '$SSH_AUTH_SOCK')" > /dev/null 2>&1
+nvim --headless --server "localhost:${1:-9090}" --remote-expr "setenv('SSH_AUTH_SOCK', '$SSH_AUTH_SOCK')"
 # accept an argument for the port, or use 9090 as the default
 nvim --server "localhost:${1:-9090}" --remote-ui
